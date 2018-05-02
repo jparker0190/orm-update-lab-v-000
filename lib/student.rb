0330618a616059a0408fb54844e13f08c800d926
Joseph Parker <jparker010190@gmail.com>
2018-05-02 02:07:35 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -49,12 +49,8 @@ def self.create(name, grade)
   stud
 end
 
-def self.new_from_db(row)
-  stud = self.new
-  stud.id = row[0]
-  stud.name = row[1]
-  stud.grade = row[2]
-  stud
+def self.new_from_db(array)
+  sql = "SELECT * from students"
 end
 
   def self.find_by_name(name)
